ba4a8ef2a0d9f1c0b17ba2462c160a6f279c7d85
adityapsvs <adityapsvs@gmail.com>
2018-10-28 23:16:39 +0530

    Back-end setup done

---
+++ server/public/precache-manifest.7a1044584a720423cbefad668d5d9d9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "346d82cfe5ebccf389e4",
+    "url": "/static/js/main.346d82cf.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "346d82cfe5ebccf389e4",
+    "url": "/static/css/main.48ccf1f2.chunk.css"
+  },
+  {
+    "revision": "661e9638e53adb9b0cdd076900d0af31",
+    "url": "/index.html"
+  }
+];
